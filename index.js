e550f4cd2a691a8cc9a40c217455b421cd556e12
Irene Ockrah <ostrene33@gmail.com>
2023-01-15 21:47:08 -0800

    introducing body-parser

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const server = express('server');
 
+const loginRequestHandler = (req, res) => {
+  console.log(req.body.email);
+  console.log(req.body.password);
+  res.send('Done');
+}
 //middleware definitions
 server.use(express.static(path.join(__dirname, 'public')));
+server.use(bodyParser.urlencoded({extended: false}));
+
+//route 
+server.post('/login', loginRequestHandler);
 
 server.listen(5000, () => console.log("Request received, wait for response"));
